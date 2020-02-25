d6b2ffe257d85bd1c2a96790fa8d69c36cfc3041
SadCherry <465462191@qq.com>
2020-02-25 16:39:48 +0800

    User authentication with Flask-Login

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 import sys
 import click
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 WIN = sys.platform.startswith('win')
 if WIN:
@@ -18,6 +20,56 @@ app.config['SECRET_KEY'] = '123456'
 
 db = SQLAlchemy(app)
 
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    user = User.query.get(int(user_id))
+    return user
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if not username or not password:
+            flash('Invalid input.')
+            return redirect(url_for('login'))
+        user = User.query.first()
+        if username == user.username and user.validate_password(password):
+            login_user(user)
+            flash('Login success')
+            return redirect(url_for('index'))
+        flash('Invalid username or password.')
+        return redirect(url_for('login'))
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Goodbye')
+    return redirect(url_for('index'))
+
+@app.cli.command()
+@click.option('--username', prompt=True, help='The username used to login.')
+@click.option('--password', prompt=True, hide_input=True, confirmation_prompt=True, help='The password used to login')
+def admin(username, password):
+    db.create_all()
+    user = User.query.first()
+    if user is not None:
+        click.echo('Updating user...')
+        user.username = username
+        user.set_password(password)
+    else:
+        click.echo('Createing user...')
+        user = User(username=username, name='Admin')
+        user.set_password(password)
+        db.session.add(user)
+    db.session.commit()
+    click.echo('Done.')
 
 @app.cli.command()
 def forge():
@@ -54,21 +106,43 @@ def initdb(drop):
     click.echo('Initialized database.')
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20))
+    username = db.Column(db.String(20))
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def validate_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(60))
     year = db.Column(db.String(4))
-
-
+@app.route('/settings', methods=['GET', 'POST'])
+@login_required
+def settings():
+    if request.method == 'POST':
+        name = request.form['name']
+        if not name or len(name) > 28:
+            flash('Invaild input')
+            return redirect(url_for('settings'))
+        current_user.name = name
+        db.session.commit()
+        flash('Settings updated.')
+        return redirect(url_for('index'))
+    return render_template('settings.html')
+    
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':  # 判断是否是 POST 请求
         # 获取表单数据
+        if not current_user.is_authenticated:
+            return redirect(url_for('index'))
         title = request.form.get('title')  # 传入表单对应输入字段的name 值
         year = request.form.get('year')
     # 验证数据
@@ -98,6 +172,7 @@ def inject_user():
     return dict(user=user)
 
 @app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+@login_required
 def edit(movie_id):
     movie = Movie.query.get_or_404(movie_id)
     if request.method == 'POST': # 处理编辑表单的提交请求
@@ -115,6 +190,7 @@ def edit(movie_id):
     return render_template('edit.html', movie=movie) # 传入被编辑的电影记录
 
 @app.route('/movie/delete/<int:movie_id>', methods=['POST']) #限定只接受 POST 请求
+@login_required
 def delete(movie_id):
     movie = Movie.query.get_or_404(movie_id) # 获取电影记录
     db.session.delete(movie) # 删除对应的记录
